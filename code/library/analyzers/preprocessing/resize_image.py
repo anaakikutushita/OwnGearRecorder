0fb35b36a979d9238d60237553b09379e7e7555a
Anaakikutsushit <atushi_anaaki@yahoo.co.jp>
2019-07-11 09:15:03 +0900

    ImageResizerのテストを追加

--- code/library/analyzers/preprocessing/resize_image.py
+++ code/library/analyzers/preprocessing/resize_image.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 """align to the same resolution before analyzing the image"""
 
+from pathlib import Path
 import cv2
 
 class ImageResizer():
@@ -20,7 +21,7 @@ class ImageResizer():
             #720pに拡縮する
             resized_image = resizer.resize(image, self.sys_resolution_x, self.sys_resolution_y)
             resized_mat_list.append(resized_image)
-        
+
         return resized_mat_list
 
 class _MatResizer():
@@ -32,3 +33,37 @@ class _MatResizer():
         Matオブジェクトを拡縮する
         """
         return cv2.resize(target_mat, (after_resized_x, after_resized_y))
+    """
+    Matオブジェクトの色を判定する。
+    本システムでは現状、白色の画素だけで構成されているかどうかのチェックにのみ使用。
+    """
+    def is_almost_white(self, target_mat, threshold):
+        """
+        渡されたMatオブジェクトが、全て白いピクセルだけで出来ていればTrueを返す。
+        ただし、thresholdの割合だけは黒いピクセルが含まれていても許容範囲とする。
+        """
+        height = target_mat.shape[0]
+        width = target_mat.shape[1]
+        black_pixels = []
+
+        # 白か黒かの判定なので、グレースケール化する。既にグレースケール化されてる場合は再度処理しない（エラーになる）
+        is_color = len(target_mat.shape) == 3
+        if is_color:
+            target_mat = cv2.cvtColor(target_mat, cv2.COLOR_BGR2GRAY)
+
+        # 値がゼロに近いほど、黒っぽい色。0～255。32という数値は適当に決めた
+        barely_white_value = 32
+
+        for h_pixel in range(height):
+            for w_pixel in range(width):
+                if target_mat[h_pixel][w_pixel] < barely_white_value:
+                    black_pixels.append(target_mat[h_pixel][w_pixel])
+        #画像が全て白になっている場合、その部位であると決定
+        return len(black_pixels) / (height * width) < threshold
+
+def test():
+    """テストでコケたときにいじくりまわすスペース"""
+    pass
+
+if __name__ == '__main__':
+    test()
--- code/unit_tests/test_resize_image.py
+++ code/unit_tests/test_resize_image.py
@@ -4,9 +4,30 @@
 from unittest import TestCase
 from pathlib import Path
 import cv2
+from ..library.analyzers.preprocessing.resize_image import ImageResizer
 from ..library.analyzers.preprocessing.resize_image import _MatResizer
 
 ASSET_FOLDER = 'code/unit_tests/assets_for_test/library/analyzers/preprocessing/resize_image/'
+
+class TestImageResizer(TestCase):
+    """ImageResizerクラス"""
+    def test_get(self):
+        """複数の解像度を持つ複数の画像を、全て同じシステム解像度に拡縮して返せるか"""
+        source_path = Path(ASSET_FOLDER + 'ImageResizer/')
+        mat_list = []
+        for img_file in list(source_path.glob('*.jpg')):
+            mat_list.append(cv2.imread(str(img_file)))
+
+        resized_list = ImageResizer().get(mat_list)
+
+        results = []
+        system_height = 720
+        system_width = 1280
+        for resized in resized_list:
+            results.append((resized.shape[0] == system_height)
+                           and (resized.shape[1] == system_width))
+
+        self.assertTrue(all(results))
 
 class TestMatResizer(TestCase):
     """_MatResizerクラス"""
