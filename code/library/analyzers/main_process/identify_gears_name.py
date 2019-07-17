01f306ca202c7531582a678811e82510943aa368
Anaakikutsushit <atushi_anaaki@yahoo.co.jp>
2019-07-17 13:01:14 +0900

    ヒストグラムベクトルを取得するロジックを分離

--- code/library/analyzers/main_process/identify_gears_name.py
+++ code/library/analyzers/main_process/identify_gears_name.py
@@ -12,29 +12,34 @@ class GearHist():
     """見本となるギア画像のヒストグラムを取得する"""
     def get_all_dict(self):
         """ギアIDとndarrayの辞書形式で各ギア画像のヒストグラムを取得する"""
-        # この辺はヒストグラム計算のために必要だから仕方なく書いてるやつ
-        all_bins = [256]
-        all_ranges = [0, 256]
-        hist_bgr = []
-        mask = None
-
         models_path = Path('images/original_model')
         model_files = list(models_path.glob('*.png'))
 
         vecs_dict = {}
         for model_file in model_files:
-            img = cv2.imread(str(model_file))
-
-            for channel in range(3):
-                hist = cv2.calcHist([img], [channel], mask, all_bins, all_ranges)
-                hist_bgr.append(hist)
-
-            hist_array = np.array(hist_bgr)
-            hist_vec = hist_array.reshape(hist_array.shape[0]*hist_array.shape[1], 1)
-
+            hist_vec = self.get_vec(model_file)
             vecs_dict[model_file.stem] = hist_vec
 
         return vecs_dict
+
+    def get_vec(self, image_file_path: Path):
+        """カラー画像ファイルを示すPathオブジェクトを受け取って、
+        三原色それぞれのヒストグラムを計算して合成したベクトルを返す"""
+        img = cv2.imread(str(image_file_path))
+
+        # この辺はヒストグラム計算のために必要だから仕方なく書いてるやつ
+        mask = None
+        all_bins = [256]
+        all_ranges = [0, 256]
+        hist_bgr = []
+        for channel in range(3):
+            hist = cv2.calcHist([img], [channel], mask, all_bins, all_ranges)
+            hist_bgr.append(hist)
+
+        hist_array = np.array(hist_bgr)
+        hist_vec = hist_array.reshape(hist_array.shape[0]*hist_array.shape[1], 1)
+
+        return hist_vec
 
 # def test():
 #     """いろいろ試せるスペース"""
