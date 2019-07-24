814e9014396e60cd4089e459bbc4714e8547c6d3
Anaakikutsushit <atushi_anaaki@yahoo.co.jp>
2019-07-24 14:49:27 +0900

    画像のクロップ前に走らせるバッチファイルを追加。将来的にはmain_systemの中に組み込むべきだと思う
    画像の上書きとかの問題で、カスタマイズ画面をクロップするには一度ファイルを削除しておく必要がある

---
+++ images/temp/output/setup.bat
@@ -0,0 +1,19 @@
+rd /S /Q Body
+rd /S /Q Foot
+rd /S /Q Head
+md Body
+md Foot
+md Head
+pushd Body
+md cutted
+md OnlyGear
+popd
+pushd Foot
+md cutted
+md OnlyGear
+popd
+pushd Head
+md cutted
+md OnlyGear
+popd
+
